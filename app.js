703fa7e1d7648b76a17b81cb760e5931c7e1efb5
Oba Seward-Evans <oba.seward@gmail.com>
2016-10-12 13:52:37 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,114 @@
+var methodOverride = require("method-override"), // allows for put() and delete()
+	firebase = require('firebase'),
+	express = require('express'),
+	bodyParser = require('body-parser');
+
+// FIREBASE * * * * * * * * * * * * * * * * * * * * * 
+require('firebase/app');
+require('firebase/auth');
+require('firebase/database');
+require('firebase/storage');
+
+firebase.initializeApp({
+	apiKey: "AIzaSyAYZ9Nb200nKhcGyHF11ABGshzcAMy5-k8",
+	authDomain: "sample-firebase-b2d4d.firebaseapp.com",
+	databaseURL: "https://sample-firebase-b2d4d.firebaseio.com",
+	storageBucket: "sample-firebase-b2d4d.appspot.com",
+	messagingSenderId: "875898499054"
+ });
+
+// The app only has access to public data as defined in the Security Rules
+var db = firebase.database();
+var ref = db.ref("/");
+
+var totalVisitors = ref.child('totalVisitors');
+totalVisitors.transaction(function (currentData) {
+	return currentData + 1;
+});
+
+var bellTowersRef = ref.child('bellTowers');
+
+// CONFIGURE APP * * * * * * * * * * * * * * * * * * * * * 
+app = express();
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+// RESTFUL ROUTES * * * * * * * * * * * * * * * * * * * * * 
+app.listen(3000, function(){
+	console.log("server is baby listening");
+});
+
+app.get("/", function(req, res){
+	res.redirect("/towers");
+});
+
+// INDEX ROUTE
+app.get("/towers", function(req, res){
+	bellTowersRef.once("value", function(towersSnapshot) {
+		// The callback succeeded.
+		res.render("index", {bellTowers: towersSnapshot});
+	}, function(error) {
+  		// The callback failed.
+  		console.error(error);
+	});
+});
+
+// NEW ROUTE
+app.get("/towers/new", function(req, res){
+	res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/towers", function(req, res) {
+	var bellTower = req.body.bellTower;
+	bellTowersRef.push(bellTower);
+	res.redirect("/");
+});
+
+// SHOW ROUTE
+app.get("/towers/:id", function(req, res){
+	bellTowersRef.child(req.params.id).once('value', function(towersSnapshot) {
+		// The callback succeeded.
+		res.render("show", {tower: towersSnapshot});
+
+	}, function(error) {
+  		// The callback failed.
+  		console.error(error);
+	});
+});
+
+// EDIT ROUTE
+app.get("/towers/:id/edit", function(req, res){
+	bellTowersRef.child(req.params.id).once('value', function(towersSnapshot) {
+		// The callback succeeded.
+		res.render("edit", {tower: towersSnapshot});
+	}, function(error) {
+  		// The callback failed.
+  		console.error(error);
+	});
+});
+
+// UPDATE ROUTE
+app.put("/towers/:id", function(req, res){ 
+	var bellTower = req.body.bellTower;
+	bellTowersRef.push(bellTower);
+
+	bellTowersRef.child(req.params.id).once('value', function(towersSnapshot) {
+		// The callback succeeded.
+		towersSnapshot = bellTower
+	}, function(error) {
+  		// The callback failed.
+  		console.error(error);
+	});
+
+	res.redirect("/towers/" + req.params.id);
+});
+
+// DESTROY ROUTE
+app.delete("/towers/:id", function(req, res){
+	console.log('delete bro', req.params.id);
+	bellTowersRef.child(req.params.id).remove();
+	res.redirect("/towers");
+});
+
