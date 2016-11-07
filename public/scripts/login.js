4097b97406a8b4a015dd2593089c231628c56ae8
Oba Seward-Evans <oba.seward@gmail.com>
2016-11-07 18:31:56 +0100

    login prompt if user not logged in && view username

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -10,8 +10,6 @@ var fb = firebase.initializeApp({
     messagingSenderId: "875898499054"
  });
 
-$('.ui.modal').modal('show');
-
 loginButton.onclick = function() {
     toggleSignIn();
 }
@@ -63,5 +61,59 @@ function toggleSignIn() {
     if (firebase.auth().currentUser) {
         $('.ui.modal').modal('hide');
         alert("welcome back!");
-    }   
+    }
 }
+
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ */
+function initApp() {
+    // Listening for auth state changes.
+    // [START authstatelistener]
+    firebase.auth().onAuthStateChanged(function(user) {
+        // [START_EXCLUDE silent]
+        /* document.getElementById('quickstart-verify-email').disabled = true; */
+        // [END_EXCLUDE]
+        if (user) {
+            // User is signed in.
+            var email = user.email;
+            var displayName = email.split("@")[0];
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            // [START_EXCLUDE silent]
+            /* document.getElementById('quickstart-sign-in-status').textContent = 'Signed in'; */
+            document.getElementById('sign-in-status').textContent = displayName; 
+            /* document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  '); */
+            if (!emailVerified) {
+                /* document.getElementById('quickstart-verify-email').disabled = false; */
+            }
+            // [END_EXCLUDE]
+        } else {
+            // User is signed out.
+            // [START_EXCLUDE silent]
+            $('.ui.modal').modal('show');
+            /* document.getElementById('quickstart-sign-in-status').textContent = 'Signed out'; */
+            document.getElementById('sign-in-status').textContent = 'Sign In'; 
+            /* document.getElementById('quickstart-account-details').textContent = 'null'; */
+            // [END_EXCLUDE]
+        }
+        // [START_EXCLUDE silent]
+        /* document.getElementById('quickstart-sign-in').disabled = false; */
+        // [END_EXCLUDE]
+    });
+
+    // [END authstatelistener]
+    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+    document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
+    document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
+    document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
+}
+
+window.onload = function() {
+    initApp();
+};
