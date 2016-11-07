9e21d8f457fd0bea29cb7cbeef4f4b615b2e2340
Oba Seward-Evans <oba.seward@gmail.com>
2016-11-07 16:19:28 +0100

    extract username and password on button press

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,7 +1,17 @@
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var loginButton = document.getElementById("loginButton");
+
+$('.ui.modal').modal('show');
+
+loginButton.onclick = function() {
+	username = $('#username').val();
+	password = $('#password').val();
+	console.log(username);
+	console.log(password);
+}
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
+btn1[0].onclick = function() {
    $('.ui.modal').modal('show');
 }
