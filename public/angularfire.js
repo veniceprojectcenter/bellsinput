55d4ea9ff8a9fc63bb210be7d8502b6ca6260956
Oba Seward-Evans <oba.seward@gmail.com>
2016-11-16 12:05:47 +0100

    sample firebase is structured just like citiknowledge

--- public/angularfire.js
+++ public/angularfire.js
@@ -29,6 +29,9 @@ var fb = firebase.initializeApp(config);
 		var previousRef;
 		
 		firebase.database().ref()
+			.child("groups")
+			.child("Bell Tower Page Final")
+			.child('members')
 			.on('value', function(towerIDs) {
 				var res = towerIDs.val();
 				console.log("Loaded", res);
@@ -39,6 +42,7 @@ var fb = firebase.initializeApp(config);
 				$scope.$apply();
 				Object.keys(bc.belltowers).forEach(function(k){
 					firebase.database().ref()
+						.child("data")
 						.child(k)
 						.on('value', function(tower){
 						// console.log("Fetch key", k, tower.val()['data']['Common name']);	
@@ -63,9 +67,9 @@ var fb = firebase.initializeApp(config);
 			bc.hideAll();
 			$('#show').show();
 			console.log(tower_id);
-			// QUICKSTART
-			// https://github.com/firebase/angularfire/blob/master/docs/quickstart.md
+			
 			bc.bell_ref = firebase.database().ref()
+				.child('data')
 				.child(tower_id)
 				.child('data');
 			bc.bell_info = $firebaseObject(bc.bell_ref);
@@ -81,17 +85,17 @@ var fb = firebase.initializeApp(config);
 			$('#edit').show();
 			console.log(tower_id);
 			setUpCategoryClicks(); // from edit.js
-			// QUICKSTART
-			// https://github.com/firebase/angularfire/blob/master/docs/quickstart.md
+			
 			if (tower_id) { // TODO: send tower_id from index to show and from show to bc.editTower(tower_id)
-				bc.bell_ref = firebase.database().ref()
+			bc.bell_ref = firebase.database().ref()
+				.child('data')
 				.child(tower_id)
 				.child('data');
 				bc.bell_info = $firebaseObject(bc.bell_ref);
 				previousRef = bc.bell_info;
 				// synchronize the object with a three-way data binding
 				// click on `index.html` above to see it used in the DOM!
-				bc.bell_info.$bindTo($scope, "bell");
+				bc.bell_info.$bindTo($scope, "bellTower");
 			}
 		};
 
@@ -112,99 +116,3 @@ var fb = firebase.initializeApp(config);
 		}
 	});
 })();
-
-
-// function unbind() {
-	/* 
-	unbind fireAngular and inputs from the previousRef.
-	*/
-
-// 	if (previousRef) { // undbind
-// 		previousRef.$destroy();
-// 	}
-// }
-
-
-// (function() {
-// 	'use strict';
-// 	var app = angular.module('app', [
-// 		// 'ngRoute',
-// 		'firebase'
-// 	]);
-	
-// 	angular.module('app').controller('BellsController', function($scope, $firebaseObject) {
-// 		var bc = this;
-// 		bc.belltowers = {};
-		
-// 		firebase.database().ref()
-// 			.child("groups")
-// 			.child("Bell Tower Page Final")
-// 			.child('members')
-// 			.on('value', function(towerIDs) {
-// 				var res = towerIDs.val();
-// 				console.log("Loaded", res);
-// 				Object.keys(res).forEach(function(k){
-// 					bc.belltowers[k] = k;
-// 				});
-// 				$scope.$apply();
-// 				Object.keys(bc.belltowers).forEach(function(k){
-// 					firebase.database().ref()
-// 						.child("data")
-// 						.child(k)
-// 						.on('value', function(tower){
-// 						// console.log("Fetch key", k, tower.val()['data']['Common name']);	
-// 						bc.belltowers[k] = tower.val()['data']['Common name'];
-// 						$scope.$apply();
-// 					});
-// 				});
-// 		});
-
-// 		$scope.$on('$includeContentLoaded', function () {
-//     		initApp();
-// 		});
-
-// 		bc.index = function(){
-// 			bc.hideAll();
-// 			$('#index').show();
-// 		};
-
-// 		bc.showTower = function(tower_id){
-// 			bc.hideAll();
-// 			$('#show').show();
-// 			console.log(tower_id);
-// 			// QUICKSTART
-// 			// https://github.com/firebase/angularfire/blob/master/docs/quickstart.md
-// 			bc.bell_ref = firebase.database().ref()
-// 				.child("data")
-// 				.child(tower_id)
-// 				.child('data');
-// 			bc.bell_info = $firebaseObject(bc.bell_ref);
-// 			// synchronize the object with a three-way data binding
-// 			// click on `index.html` above to see it used in the DOM!
-// 			bc.bell_info.$bindTo($scope, "bell");
-// 		};
-		
-// 		bc.editTower = function(tower_id){
-// 			bc.hideAll();
-// 			$('#edit').show();
-// 			console.log(tower_id);
-// 			setUpCategoryClicks(); // from edit.js
-// 			// QUICKSTART
-// 			// https://github.com/firebase/angularfire/blob/master/docs/quickstart.md
-// 			bc.bell_ref = firebase.database().ref()
-// 				.child("data")
-// 				.child(tower_id)
-// 				.child('data');
-// 			bc.bell_info = $firebaseObject(bc.bell_ref);
-// 			// synchronize the object with a three-way data binding
-// 			// click on `index.html` above to see it used in the DOM!
-// 			bc.bell_info.$bindTo($scope, "bell");
-// 		};
-
-// 		bc.hideAll = function() {
-// 			$('#index').hide();
-// 			$('#edit').hide();
-// 			$('#show').hide();
-// 		};
-// 	});
-// })();
